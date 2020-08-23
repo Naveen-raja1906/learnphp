2b447f7f00b0f2ec2e1bad850c5d8c21b9b48aa1
Naveen Raja <dnaveenraja@gmail.com>
2020-08-23 12:32:03 +0530

    first commit

---
+++ cred.php
@@ -0,0 +1,7 @@
+<? php
+
+	$name = $_GET['name'];
+	$pass = $_GET['password'];
+	echo 'name ='.$name;
+	echo 'pass ='.$pass;
+?>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Malicious page</title>
+</head>
+<body>
+
+<form action="cred.php">
+	Email:		<input type="email" name="email">
+	Password:	<input type="Password" name="password">
+	<button>Submit</button>
+</form>
+
+<h2>Download Image here</h2>
+<a href="index.php?file=pic.jpg">
+
+
+<? php
+		
+	if(!empty($_GET['file'])){
+
+		$filename = basename($_GET['file']);
+		$filepatch = 'image/'.$filename;
+
+		if(!empty($filename) && file_exists($filepatch)){
+
+			//Define Headers
+			header("Cache-Control: public");
+			header("Content-Description: File Transfer");
+			header("Content-Disposition: attachment; filename=$filename");
+			header("Content-Type: application/zip");
+			header("Content-Transfer-Emcoding: binary");
+
+			readfile($filepatch);
+			exit;
+		}else{
+			echo "No File found";
+		}
+	}
+
+?>
+
+</body>
+</html>
